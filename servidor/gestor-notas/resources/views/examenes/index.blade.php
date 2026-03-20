0c5431b640b62165a4ed6b9654a26aed92d59599
jponper533 <jponper533@gmail.com>
2026-03-20 12:17:04 +0100

    borrado server

--- servidor/gestor-notas/resources/views/examenes/index.blade.php
+++
@@ -1,7 +0,0 @@
-<h1>Lista de examen</h1>
-@if(auth()->user()->role_id != 3)
-<p><a href="{{ route('examen.create') }}">Crear</a></p>
-@endif
-@include('examenes._list')
-
-<a href="{{ route('dashboard') }}">Volver</a>
--- servidor/gestor-notas/resources/views/home/index.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('layouts.main')
-
-@section('title', 'Home')
-
-@section('content')
-    <h1>HOME - GESTOR DE examen </h1>
-    <p><a href="{{ route('login') }}">Login</a></p>
-@endsection
