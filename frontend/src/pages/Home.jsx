f702191afea622678895087a234e55d91f7bafc9
jponper533 <jponper533@gmail.com>
2026-04-07 12:04:59 +0200

    probando con la api

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,32 +2,43 @@ import styles from "./home.module.css";
 import { useState, useEffect } from "react";
 
 function Home() {
-  const [usuarios, setUsuarios] = useState([]); // usamos array en lugar de null
+    const [usuarios, setUsuarios] = useState([]); // usamos array en lugar de null
 
-  useEffect(() => {
-    fetch("http://localhost:8000/api/usuariosApi")
-      .then((response) => response.json())
-      .then((json) => setUsuarios(json))
-      .catch((error) => console.error(error));
-  }, []);
+    useEffect(() => {
+        fetch("http://localhost:8000/api/usuariosApi")
+            .then((response) => response.json())
+            .then((json) => setUsuarios(json))
+            .catch((error) => console.error(error));
+    }, []);
 
-  return (
-    <main className={styles.main}>
-      <h1>Bienvenido X</h1>
-      <h2>Lista de Usuarios:</h2>
-      {usuarios.length === 0 ? (
-        <p>No hay usuarios disponibles</p>
-      ) : (
-        <ul>
-          {usuarios.map((usuario) => (
-            <li key={usuario.id}>
-              {usuario.name} - {usuario.email}
-            </li>
-          ))}
-        </ul>
-      )}
-    </main>
-  );
+    function getUsuario(id) {
+        return fetch(`http://localhost:8000/api/usuariosApi/${id}`)
+            .then(res => res.json());
+    }
+
+    useEffect(() => {
+        getUsuario(2) // traer usuario con id = 2
+            .then(usuario => console.log(usuario))
+            .catch(err => console.error(err));
+    }, []);
+
+    return (
+        <main className={styles.main}>
+            <h1>Bienvenido X</h1>
+            <h2>Lista de Usuarios:</h2>
+            {usuarios.length === 0 ? (
+                <p>No hay usuarios disponibles</p>
+            ) : (
+                <ul>
+                    {usuarios.map((usuario) => (
+                        <li key={usuario.id}>
+                            {usuario.name} - {usuario.email}
+                        </li>
+                    ))}
+                </ul>
+            )}
+        </main>
+    );
 }
 
 export default Home;
